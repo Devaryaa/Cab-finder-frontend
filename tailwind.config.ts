fe296dd0091ba73691eff1d387ad0cd11b297fb6
Devaryaa <devarya212006@gmail.com>
2025-06-23 19:56:04 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // 👈 FIXED PATHS
   theme: {
     extend: {
       borderRadius: {
@@ -88,3 +88,4 @@ export default {
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
+
